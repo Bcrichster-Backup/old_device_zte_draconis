59426429ae81767b0c6ca7f9957d4574385517f4
hroark13 <hroark13@yahoo.com>
2016-03-29 02:44:49 -0400

    alpha2

--- broken.mk
+++ broken.mk
@@ -4,10 +4,5 @@ $(call inherit-product, device/zte/draconis/full_draconis.mk)
 $(call inherit-product, vendor/broken/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := ZTE ZMAX
-
-    PRODUCT_NAME := broken_draconis
-    PRODUCT_COPY_FILES += \
-        vendor/broken/prebuilt/common/bootanimation/720.zip:system/media/bootanimation.zip
-
-
+PRODUCT_NAME := broken_draconis
 PRODUCT_PACKAGES += OTAUpdates
