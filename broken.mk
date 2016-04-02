4c5c31a9aeadf0410a7a27a7487eb20e7de328bd
dfuse06 <dfuse06@gmail.com>
2016-04-01 21:04:26 -0500

    Broken Device Maintainers

--- broken.mk
+++ broken.mk
@@ -6,3 +6,7 @@ $(call inherit-product, vendor/broken/config/common_full_phone.mk)
 PRODUCT_RELEASE_NAME := ZTE ZMAX
 PRODUCT_NAME := broken_draconis
 PRODUCT_PACKAGES += OTAUpdates
+
+# Broken Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+DEVICE_MAINTAINERS="Dustin Winings (D-FUSE)" 
