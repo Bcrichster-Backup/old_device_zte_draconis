6cba1f007dd74d4bd096674e4811a4a17791f6ed
dfuse06 <dfuse06@gmail.com>
2016-04-09 08:48:35 -0500

    Fix

--- broken.mk
+++ broken.mk
@@ -3,10 +3,16 @@ $(call inherit-product, device/zte/draconis/full_draconis.mk)
 # Inherit some common broken stuff.
 $(call inherit-product, vendor/broken/config/common_full_phone.mk)
 
-PRODUCT_RELEASE_NAME := ZTE ZMAX
-PRODUCT_NAME := broken_draconis
-PRODUCT_PACKAGES += OTAUpdates
+PRODUCT_BUILD_PROP_OVERRIDES += \
+PRODUCT_NAME=draconis
 
 # Broken Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
 DEVICE_MAINTAINERS="Dustin Winings (D-FUSE)" 
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := draconis
+PRODUCT_NAME := broken_draconis
+PRODUCT_BRAND := zte
+PRODUCT_MODEL := draconis
+PRODUCT_MANUFACTURER := zte
