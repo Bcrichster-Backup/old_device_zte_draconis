f8fc535688bd88271e6f05c3f4a9789f2ebe6980
D-FUSE <dfuse06@gmail.com>
2016-04-20 21:00:00 -0500

    Update broken.mk

--- broken.mk
+++ broken.mk
@@ -3,10 +3,6 @@ $(call inherit-product, device/zte/draconis/full_draconis.mk)
 # Inherit some common broken stuff.
 $(call inherit-product, vendor/broken/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-PRODUCT_NAME=draconis \
-PRODUCT_MODEL=draconis
-
 # Broken Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
 DEVICE_MAINTAINERS="Dustin Winings (D-FUSE)" 
@@ -15,5 +11,9 @@ DEVICE_MAINTAINERS="Dustin Winings (D-FUSE)"
 PRODUCT_DEVICE := draconis
 PRODUCT_NAME := broken_draconis
 PRODUCT_BRAND := zte
-PRODUCT_MODEL := draconis
+PRODUCT_MODEL := Z970
 PRODUCT_MANUFACTURER := zte
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+PRODUCT_NAME="draconis" \
+PRODUCT_MODEL="Z970"
