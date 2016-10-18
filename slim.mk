7659efc9ebc76eb1f04d7f756ec6ad7fbe8fe31e
Tyler <tyler.android12@gmail.com>
2016-10-18 07:22:19 -0400

    slimified

--- slim.mk
+++ slim.mk
@@ -3,10 +3,6 @@ $(call inherit-product, device/zte/draconis/full_draconis.mk)
 # Inherit some common broken stuff.
 $(call inherit-product, vendor/broken/config/common_full_phone.mk)
 
-# Broken Device Maintainers
-PRODUCT_BUILD_PROP_OVERRIDES += \
-DEVICE_MAINTAINERS="Dustin Winings (D-FUSE)" 
-
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := draconis
 PRODUCT_NAME := broken_draconis
