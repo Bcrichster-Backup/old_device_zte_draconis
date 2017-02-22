3d620043ec675b594a4860c89ed4c8091f509a82
Tyler <tyler.android12@gmail.com>
2017-02-21 19:49:00 -0500

    Update and rename broken.mk to lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -1,15 +1,15 @@
 $(call inherit-product, device/zte/draconis/full_draconis.mk)
 
-# Inherit some common broken stuff.
-$(call inherit-product, vendor/broken/config/common_full_phone.mk)
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Broken Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
-DEVICE_MAINTAINERS="Dustin Winings (D-FUSE)" 
+DEVICE_MAINTAINERS="Tyler Adams (Relyt2012)" 
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := draconis
-PRODUCT_NAME := broken_draconis
+PRODUCT_NAME := lineage_draconis
 PRODUCT_BRAND := zte
 PRODUCT_MODEL := Z970
 PRODUCT_MANUFACTURER := zte
